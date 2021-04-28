6de53a8a3b83bbcdfed980320361d15da86921ff
Juan David <juand8012@gmail.com>
2021-04-27 21:45:21 -0500

    Animals section styles are working, but cat button doesnt work well yet

--- src/select-pet.js
+++ src/select-pet.js
@@ -1,5 +1,6 @@
 const petDescription = document.querySelector('.pet-descriptions');
 const dogButton = document.querySelector('#pet-section');
+const catButton = document.querySelector('#cat-section');
 
 const pet = [
     {name: "Chester", breed: "Golden retriever"},
@@ -8,11 +9,42 @@ const pet = [
     {name:"Pelusa", breed:"Bichón frisé"}
 ]
 
-console.log(pet);
-
-dogButton.addEventListener('click',  (e) =>{
-    console.log(petDescription);
-    petDescription.innerHTML += `
-        <img src="../assets/images/perro.svg" alt="test">; 
+dogButton.addEventListener('click',  () =>{
+    let asideSection = document.createElement("section");
+    asideSection.className = "dogs-section"
+    asideSection.innerHTML = ` 
+        <article class="pet-article">
+            <img src="../assets/images/Chester.svg" alt="Chester">
+        </article>
+        <article class="pet-article">
+            <img src="../assets/images/Papi.svg" alt="Papi">
+        </article> 
+        <article class="pet-article">
+            <img src="../assets/images/Rocky.svg" alt="Rocky">
+        </article> 
+        <article class="pet-article">
+            <img src="../assets/images/Pelusa.svg" alt="Pelusa">
+        </article>  
     `
+    petDescription.appendChild(asideSection);
+});
+
+catButton.addEventListener('click',  () => {
+    let asideSection = document.createElement("section");
+    asideSection.className = "cats-section"
+    asideSection.innerHTML = ` 
+        <article class="pet-article">
+            <img src="../assets/images/Matilde.svg" alt="Matilde">
+        </article>
+        <article class="pet-article">
+            <img src="../assets/images/PelusaGato.svg" alt="Pelusa">
+        </article> 
+        <article class="pet-article">
+            <img src="../assets/images/Kitty.svg" alt="Kitty">
+        </article> 
+        <article class="pet-article">
+            <img src="../assets/images/Bombón.svg" alt="Bombon">
+        </article>  
+    `
+    petDescription.appendChild(asideSection);
 });
